2c4adc059060412b11dc034e814063521d6aaaa4
Kushwanth Masupalli <masupallikushwanth2005@gmail.com>
2026-04-17 19:47:37 +0530

    corrected path

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-const WebhookEvent = require("./models/WebhookEvent");
+const WebhookEvent = require("WebhookEvent");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
